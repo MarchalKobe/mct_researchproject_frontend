086ebd19581c4f36292a70503bd6ca8e87c0592d
MarchalKobe <kobe.marchal@student.howest.be>
2022-01-14 21:29:16 +0100

    Added navbar information styling

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -25,18 +25,18 @@
                 <circle id="Ellipse_3" data-name="Ellipse 3" cx="4" cy="4" r="4" transform="translate(15.999 16)" fill="#fff"/>
             </svg>
     
-            <RouterLink class="c-button__icon" :class="path === '/classes' ? 'c-button__icon-selected' : ''" to="/classes">
+            <RouterLink class="c-navbar__element c-button__icon" :class="path === '/classes' ? 'c-button__icon-selected' : ''" to="/classes" data-information="Home">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
             </RouterLink>
         </div>
 
         <div class="u-flex u-direction-column u-align-center">
-            <button class="c-button__icon c-button__icon-alpha u-margin-bottom-md" @click="logout">
+            <button class="c-navbar__element c-button__icon c-button__icon-alpha u-margin-bottom-md" @click="logout" data-information="Log out">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M10 3H6a2 2 0 0 0-2 2v14c0 1.1.9 2 2 2h4M16 17l5-5-5-5M19.8 12H9"/></svg>
             </button>
 
             <div class="c-navbar__profile">
-                <RouterLink class="c-button__soft" to="/profile">
+                <RouterLink class="c-navbar__element c-button__soft" to="/profile" data-information="Profile">
                     <div class="c-navbar__avatar" v-html="user.information.avatar"></div>
                 </RouterLink>
             </div>
